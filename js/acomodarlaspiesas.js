43f5b60b75fc17c28141002fda5bc80244c4199e
mariano <marianoapanelo@gmail.com>
2023-04-02 19:22:08 -0300

    empezando la parte de arreglar las imagenes

---
+++ js/acomodarlaspiesas.js
@@ -0,0 +1,8 @@
+/*dejo esto porque en un futuro voy a intentar hacer que se vean 
+las fotos y deja la pagina completa */
+
+let ariba = document.getElementById("ariba")
+let medio = document.getElementById("medio")
+let abajo = document.getElementById("abajo")
+
+
