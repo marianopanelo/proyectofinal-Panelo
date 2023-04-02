c27f7c5d4a3fe1ca38359fc01469ad6257d6df87
mariano <marianoapanelo@gmail.com>
2023-04-02 19:07:27 -0300

    agregue el juego de adivinar el numero y le puse notificaciones a las cosas

--- js/apuestas.js
+++ js/apuestas.js
@@ -1,95 +1,109 @@
 
 /*juego*/
-let DineroInicial = Number(prompt("indique cuanto dinero posee"))
-let apuesta = Number(prompt("indique cuanto desea apostar"))
-let numeroGanador = Number(Math.floor(Math.random() * (0 + 100)))
-let ciclos = 1
 
-for (let intentos = 0 ; intentos <= 20 ; intentos++){
+let apuesta = document.getElementById("tragosAApostar")
+apuesta.addEventListener ("click" , function (e){
+    let prueba = e.target.parentNode
+    let cantidad_apostada = Number(prueba.querySelector("input").value);
+    console.log(cantidad_apostada)
+    prueba.remove()
 
-    let numeroElegido = Number(prompt("elija un numero de 0 a 100"))
 
-    if  (numeroElegido == numeroGanador) { 
-        alert  ("felicidades encontraste el numero")
-       break
-    }
-
-    else if ((numeroElegido + 6) > numeroGanador && numeroElegido  < numeroGanador ) {
-        alert ("el numero ganador esta entre los 5 numeros siguientes")
-    }
-
-   else if ((numeroElegido - 6) < numeroGanador && numeroElegido > numeroGanador ) {
-        alert ("el numero ganador esta entre los 5 numeros anteriores")
-    }
-
-    else if (numeroElegido > numeroGanador ) {
-        alert ("el numero ganador es menor")
-    }
-
-    else if (numeroElegido < numeroGanador) {
-        alert ("el numero ganador es mayor")
-    }
-
-    else {
-        alert ("no eligio un numero , por favor vuelva a intentarlo")
-    }
-    ciclos++
-}
+    let numeroGanador = Number(Math.floor(Math.random() * (0 + 100)))
+    console.log(numeroGanador)
+    let numero_elejido = document.getElementById("numeroApostado")
+    let respuestas = document.getElementById("respuestas")
+    let intentos = 0
+  
 
     
-/*le tube que poner ciclos++ poruqe necesitaba sacar las cantidad de vueltas del ciclo y si ponia abajo intentos ,que seria la cantidad de siclos
-no me lo tomaba , supongo que es porque cuando hago un break me corta los siclos y no me los cuenta*/
-    
-alert ("el numero ganador era " + numeroGanador)
+
+    numero_elejido.addEventListener ("click" , function (e){
+        intentos ++
+        let prueba_dos = e.target.parentNode
+        let numero_apuesta = Number(prueba_dos.querySelector("input").value);
+            
+        console.log (intentos)
+            if  (numero_apuesta == numeroGanador) { 
+                let respuesta = document.createElement("p")
+                respuesta.innerHTML= `intento numero  ${intentos} 
+                                    su numero fue ${numero_apuesta}
+                                    felicidades encontraste el numero
+                                    `  
+                respuestas.append(respuesta)
+                prueba_dos.remove ()
+                function tragos_ganados (){
+
+                    if (intentos <= 2){
+                        cantidad_apostada *= 3
+                        respuesta.innerHTML = `felicidades, encontro el numeor por lo cual das ${cantidad_apostada} shot `
+                    }
+                
+                    else if (intentos <= 5  && intentos > 2){
+                        cantidad_apostada  *= 1.5
+                        respuesta.innerHTML = `felicidades, encontro el numeor por lo cual das ${cantidad_apostada} shot `
+                    }
+                
+                    else if (intentos <= 7 && intentos > 5){
+                         
+                        respuesta.innerHTML = ` encontro el numeor, por lo cual no toma ni das shot`
+                    }
+                
+                    else {
+                         
+                        respuesta.innerHTML = ` encontro el numeor tarde, te toca tomar ${cantidad_apostada} shot`
+                    }                
+                }
+                
+                tragos_ganados()
+            }
+            
+            else if ((numero_apuesta + 6) > numeroGanador && numero_apuesta  < numeroGanador ) {
+                let respuesta = document.createElement("p")
+                respuesta.innerHTML= `intento numero  ${intentos} 
+                                    su numero fue ${numero_apuesta}
+                                    el numero ganador esta entre los 5 numeros siguientes`
+                respuestas.append(respuesta)
+
+            }
+            
+            else if ((numero_apuesta - 6) < numeroGanador && numero_apuesta > numeroGanador ) {
+                let respuesta = document.createElement("p")
+                respuesta.innerHTML= `intento numero  ${intentos}
+                                    su numero fue ${numero_apuesta}
+                                    el numero ganador esta entre los 5 numeros anteriores`
+                respuestas.append(respuesta)
 
 
-function cuentas (operacion , num1, num2){
-    return operacion (num1, num2)
-}
-    
-function resta (a,b){
-    return a - b  
-}
-    
-function suma2 (a, b){
-    return a + b
-}
+            }
+            
+            else if (numero_apuesta > numeroGanador ) {
+                let respuesta = document.createElement("p")
+                respuesta.innerHTML= `intento numero  ${intentos}
+                                        su numero fue ${numero_apuesta}
+                                        el numero ganador es menor`
+                respuestas.append(respuesta)
+                
+
+            }
+            
+            else if (numero_apuesta < numeroGanador) {
+                let respuesta = document.createElement("p")
+                respuesta.innerHTML= `intento numero  ${intentos}
+                                        su numero fue ${numero_apuesta}    
+                                        el numero ganador es mayor`
+                respuestas.append(respuesta)
 
 
+            }
+            
+            else {
+                let respuesta = document.createElement("p")
+                respuesta.innerHTML= `intento numero  ${intentos}
+                                        su numero fue ${numero_apuesta}        
+                                        no eligio un numero , por favor vuelva a intentarlo`
+                respuestas.append(respuesta)
+            }
 
-
-function cantidadGanada (porcentaje){
-    
-    if (ciclos <= 2){
-        porcentaje = 3
-        let suma = (apuesta * porcentaje)
-    }
-    
-    else if (ciclos <= 5  && ciclos > 2){
-        porcentaje = 1.5
-        let suma = (apuesta * porcentaje)
-    }
-    
-    else if (ciclos <= 7 && ciclos > 5){
-        porcentaje = 1
-        let suma = (apuesta * porcentaje)
-    }
-    
-    else {
-        porcentaje = 0
-        let suma = (apuesta * porcentaje)
-    }
-    
-    let suma = (apuesta * porcentaje)
-
-    alert ("usted gano " + suma + "$" )
-    
-    
-    
-    let resta2 = cuentas (resta , DineroInicial, apuesta)
-
-    alert (  "a usted le quedaban " + (cuentas (resta , DineroInicial, apuesta)) + "$")
-    alert (  "a usted le quedan " + (cuentas (suma2 , resta2, suma)) + "$")
-
-}
-cantidadGanada()
+    })
+})
